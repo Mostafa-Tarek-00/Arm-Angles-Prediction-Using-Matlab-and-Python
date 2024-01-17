83efc2ea59fbd8cb1119f6e2860add5b845b579a
Mostafa Tarek Gaber <76787661+Mostafa-Tarek-00@users.noreply.github.com>
2024-01-17 22:14:53 +0200

    Add files via upload

---
+++ Matlab/G6_inv_newff.m
@@ -0,0 +1,66 @@
+%clear all
+x = xlsread('train.xlsx');
+y = xlsread('test.xlsx');
+format longe
+
+inptrain = [x(7, :); x(8, :); x(9, :)];
+inptest = [y(7, :); y(8, :); y(9, :)];
+
+outtrain = [x(1, :); x(2, :); x(3, :); x(4, :); x(5, :); x(6, :)];
+outtest = [y(1, :); y(2, :); y(3, :); y(4, :); y(5, :); y(6, :)];
+
+% Feed Forward
+%hiddenLayerSize = [30, 20, 10]; 
+%trainFcn = 'trainlm';  
+%net = fitnet(hiddenLayerSize, trainFcn);
+
+%net.input.processFcns = {'removeconstantrows','mapminmax'};
+%net.output.processFcns = {'removeconstantrows','mapminmax'};
+
+%net.layers{1}.transferFcn = 'tansig';
+%net.performParam.regularization = 0.001; 
+
+%net.trainParam.lr = 0.00001;
+
+%net.trainParam.epochs = 1000;
+
+%net.trainParam.batchSize = 16;
+
+%net1 = train(net, inptrain, outtrain);
+
+
+%save('net_new_ff.mat', 'net1');
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%5
+load('net_new_ff.mat');
+
+p1 = sim(net1, inptrain);
+error1 = p1 - outtrain;
+
+input_size = net1.inputs{1}.size;
+output_size = net1.outputs{end}.size;
+
+fprintf('Input Size of the Neural Network: %d\n', input_size);
+fprintf('Output Size of the Neural Network: %d\n', output_size);
+
+predictions = sim(net1, inptest);
+
+mseValue = mse(outtest, predictions);
+
+maeValue = mae(outtest, predictions);
+
+rmseValue = sqrt(mseValue);
+
+fprintf('Mean Squared Error (MSE): %.4f\n', mseValue);
+fprintf('Mean Absolute Error (MAE): %.4f\n', maeValue);
+fprintf('Root Mean Squared Error (RMSE): %.4f\n', rmseValue);
+
+fprintf('\nTrue and Predicted Values for the First 5 Data Points:\n');
+for i = 1:5
+    fprintf('Data Point %d\n', i);
+    fprintf('Input Values: %s\n', mat2str(inptest(:, i)));
+    fprintf('True Values: %s\n', mat2str(outtest(:, i)));
+    fprintf('Predicted Values: %s\n', mat2str(predictions(:, i)));
+    fprintf('------------------------\n');
+end
---
+++ Matlab/graph_test.m
@@ -0,0 +1,31 @@
+
+x = readtable('test.xlsx');
+
+out = table2array(x(1:6,:));
+inp = table2array(x(7:9,:));
+
+load('net_new_ff.mat', 'net1');
+outputs1 = sim(net1, inp);
+error1 = out - outputs1;
+
+load('net_new_grnn.mat','net2');
+
+outputs2 = sim(net2, inp);
+error2 = out - outputs2;
+
+load('net_new_rb.mat', 'net3');
+
+outputs3 = sim(net3, inp);
+error3 = out - outputs3;
+
+
+for i = 1:size(out, 1)
+    figure(i)
+    cc = 1:size(out, 2);
+    plot(cc, out(i,:), 'ko', cc, outputs1(i,:), 'r^', cc, outputs2(i,:), 'xb', cc, outputs3(i,:), '*g');
+    xlabel('No.'), ylabel(['Output ', num2str(i)]);
+    legend('COMSOL results', 'fitted NNFF (net2)','fitted NEWGRNN', 'fitted NEWRB (net3)');
+    title(['Output ', num2str(i), ' from COMSOL and Neural Networks']);
+    saveas(gcf, ['output_test_', num2str(i), '.jpg']);
+end
+
